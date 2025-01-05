c061b0c676c34fd9f545512f6968df96e2e29675
rs545837 <114828377+rs545837@users.noreply.github.com>
2025-01-05 06:00:25 -0800

    Update model_v12.py

--- model_v12.py
+++ model_v12.py
@@ -378,9 +378,9 @@ class Block(nn.Module):
         self.use_parallel_residual = config.use_parallel_residual
         self.use_scaled_residual = config.use_scaled_residual
         
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
+        self.ln_1 = get_norm(config)
         self.attn = get_attention_mechanism(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
+        self.ln_2 = get_norm(config)
         self.mlp = MLP(config)
         
         if self.use_scaled_residual:
@@ -474,7 +474,7 @@ class GPT(nn.Module):
             # wpe = nn.Embedding(config.block_size, config.n_embd),
             drop = nn.Dropout(config.dropout),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+            ln_f = get_norm(config),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         # with weight tying when using torch.compile() some warnings get generated:
